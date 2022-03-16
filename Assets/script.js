87452e0822e10861862e649fd89e60a5fce99843
George Geils <95538913+ggeils@users.noreply.github.com>
2022-03-15 23:21:10 -0400

    Created function to start quiz in script.js

--- Assets/script.js
+++ Assets/script.js
@@ -1,42 +1,75 @@
 const questions = [
-    {question: "Commonly used data types DO NOT include:", 
-    options: ["strings", "booleans", "alerts", "numbers"], 
-    answer: "alerts"},
+  {
+    question: "Commonly used data types DO NOT include:",
+    options: ["strings", "booleans", "alerts", "numbers"],
+    answer: "alerts",
+  },
 
-    {question: "The condition in an if / else statement is enclosed within _____.", 
-    options: ["quotes", "curly brackets", "parentheses", "square brackets"], 
-    answer: "parentheses"},
+  {
+    question:
+      "The condition in an if / else statement is enclosed within _____.",
+    options: ["quotes", "curly brackets", "parentheses", "square brackets"],
+    answer: "parentheses",
+  },
 
-    {question: "Arrays in Javascript can be used to store _____.", 
-    options: ["numbers & strings", "other arrays", "booleans", "all of the above"],
-    answer: "all of the above"},
+  {
+    question: "Arrays in Javascript can be used to store _____.",
+    options: [
+      "numbers & strings",
+      "other arrays",
+      "booleans",
+      "all of the above",
+    ],
+    answer: "all of the above",
+  },
 
-    {question: "String values must be enclosed within _____ when being assigned to letiables.", 
-    options: ["commas", "curly brackets", "quotes", "parantheses"], 
-    answer: "quotes"},
+  {
+    question:
+      "String values must be enclosed within _____ when being assigned to letiables.",
+    options: ["commas", "curly brackets", "quotes", "parantheses"],
+    answer: "quotes",
+  },
 
-    {question: "A very useful tool used during development and debugging for printing content to the debugger is:", 
-    options: ["Javascript", "terminal/bash", "for loops", "console.log"], 
-    answer: "terminal/bash"}
+  {
+    question:
+      "A very useful tool used during development and debugging for printing content to the debugger is:",
+    options: ["Javascript", "terminal/bash", "for loops", "console.log"],
+    answer: "terminal/bash",
+  },
 ];
 
-let userAnswers = ["","","","",""];
+let userAnswers = ["", "", "", "", ""];
 let correctAnswers = 0;
 let score = "";
 let questionInd = 0;
 let startTimer = 0;
 let secondsLeft = 120;
-let questionPrompt = $('#question-prompt');
-let ansContainEl = $('#answer-buttons');
-let startButton = $('#start-btn');
-let previousButton = $('#prev-btn');
-let nextButton = $('#next-btn');
-let submitButton = $('#submit-btn');
+let questionPrompt = $("#question-prompt");
+let ansContainEl = $("#answer-buttons");
+let startButton = $("#start-btn");
+let previousButton = $("#prev-btn");
+let nextButton = $("#next-btn");
+let submitButton = $("#submit-btn");
 
-$(ansContainEl).on('click',selectAns)
-$(startButton).on('click',startQuiz)
-$(nextButton).on('click',nextQuest)
-$(previousButton).on('click',prevQuest)
-$(submitButton).on('click',results)
+$(ansContainEl).on("click", selectAns);
+$(startButton).on("click", Start);
+$(nextButton).on("click", nextQuest);
+$(previousButton).on("click", prevQuest);
+$(submitButton).on("click", results);
 
-
+function Start() {
+  startTimer = 1;
+  randomizeQuest = shuffle(questions);
+  $(startButton).addClass("hide");pt
+  $(questionPrompt).text(randomizeQuest[questionInd].question);
+  questions[questionInd].options.forEach((ans) => {
+    let button = $("<button>");
+    let buttonID = uniqueId(ans);
+    $(button).addClass("btn btn-primary btn-block chosen");
+    $(button).attr("id", buttonID);
+    $(button).text(ans);
+    $("#answer-buttons").append(button);
+  });
+  $(ansContainEl).removeClass("hide");
+  $(nextButton).removeClass("hide");
+}
